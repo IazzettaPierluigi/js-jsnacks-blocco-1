dfce0ae108be9b11bdaf7687f5bf3bbc9086c501
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-01-11 16:23:10 +0100

    blocco 1 js-6 DONE

--- blocco1/jsnak-6/main.js
+++ blocco1/jsnak-6/main.js
@@ -1,1 +1,14 @@
-console.log("ciao")
+// -Stampa il cubo dei primi N numeri,
+// dove N è un numero indicato dall’utente.
+
+// creo la variabile dal prompt
+let numeroUser = prompt("inserisci un numero")
+
+
+
+//uso il for per stampare i cubi dei numeri fino al numero indicato
+for (let i = 0; i <= numeroUser; i++){
+    let cubo = i ** 3; //calcolo del cubo
+    document.writeln(`il cubo di ${i} è ${cubo}`)
+    
+}
