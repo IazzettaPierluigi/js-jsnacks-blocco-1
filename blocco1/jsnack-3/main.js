19e14059613c0a9cc6fc89b4ed9851a1945daab8
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-01-11 15:58:20 +0100

    b1-js3 DONE

--- blocco1/jsnack-3/main.js
+++ blocco1/jsnack-3/main.js
@@ -1,1 +1,13 @@
-console.log("ciao")
+// Blocco 1 - JSnack 3
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+
+let somma = 0
+
+for (let i = 1; i <= 10; i++) {
+    let input = prompt("inserisci un numero");
+
+    somma += +input;
+}
+
+console.log(`la somma è ${somma}`)
