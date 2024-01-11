3bdbc5e1838cc8fc9de5112c358a0bea5e57d933
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-01-11 16:39:51 +0100

    blocco2 jsnack-2 DONE

--- blocco2/jsnack-2/main.js
+++ blocco2/jsnack-2/main.js
@@ -1,1 +1,49 @@
-console.log("ciao")
+// Generatore di “nomi cognomi” casuali: il Grande Gatsby ha una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome.
+
+//creo l'array dei nomi
+const nomiPersone = [
+    "Luke",
+    "Leia",
+    "Han",
+    "Indiana",
+    "Frodo",
+    "Aragorn",
+    "Neo",
+    "Trinity",
+    "Marty",
+    "Ellen"
+];
+
+//creo l'array dei cognomi 
+const cognomiPersone = [
+    "Skywalker",
+    "Organa",
+    "Solo",
+    "Jones",
+    "Baggins",
+    "Elessar",
+    "Anderson",
+    "Moss",
+    "McFly",
+    "Ripley"
+  ];
+
+
+  //creo un array da riempire col push dopo aver unito cognome e nome
+  const listaPersone = []; 
+
+  for (let i = 0; i <10; i++) {
+    //nome random
+    let nomeRandom = nomiPersone[Math.floor(Math.random() * nomiPersone.length)];
+
+    //cognome random
+    let cognomeRandom = cognomiPersone[Math.floor(Math.random() * cognomiPersone.length)];
+
+    let personaRandom = `${nomeRandom} ${cognomeRandom} <br>`;
+
+    //faccio il push di personaRandom nell'array listaPersone
+
+    listaPersone.push(personaRandom);
+  }
+
+  document.writeln(`ecco le persone invitate: ${listaPersone}`)
